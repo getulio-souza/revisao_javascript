fe1fe05535f953051767bec43862ab95f182ff62
getulio.souza <getulio.souza@evolutix.com.br>
2023-05-19 15:59:27 -0300

    new

--- toLocaleString.js
+++ toLocaleString.js
@@ -1,5 +1,22 @@
-let myValue = 123456.789
+// let myValue = 123456.789
 
-myValue = myValue.toLocaleString("us-US", {style: 'currency', currency: "USD"});
+// myValue = myValue.toLocaleString("us-US", {style: 'currency', currency: "USD"});
+
+// console.log(myValue)
+
+let myValue = 100
+
+myValue = myValue.toLocaleString(undefined, {style: 'percent', unit: "celsius"});
 
 console.log(myValue)
+
+
+
+
+
+
+
+
+
+
+
