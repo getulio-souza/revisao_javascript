773526cfd58549c152a02e44e9cf4adb28051723
getulio.souza <getulio.souza@evolutix.com.br>
2023-05-21 22:52:29 -0300

    feature: guess-game

--- if-else.js
+++ if-else.js
@@ -1,15 +1,38 @@
-const a = 7
-const b = 3
-const c = 8
-const menor = number = 0
+// const a = 7
+// const b = 3
+// const c = 8
+// const menor = number = 0
 
-if (a < b && a < c) {
-  console.log(`o valor menor é ${a}`);
-} else {
-  if (b < c) {
-    console.log(`o valor menor é ${b}`);
+// if (a < b && a < c) {
+//   console.log(`o valor menor é ${a}`);
+// } else {
+//   if (b < c) {
+//     console.log(`o valor menor é ${b}`);
+//   } else {
+//     console.log(`o valor menor é ${c}`);
+//   }
+// }
+
+// function isEvenOrOdd(num) {
+//   if (num % 2 == 0) {
+//     console.log(`${num} is a odd number`)
+//   } else {
+//     console.log(`${num} is even number`)
+//   }
+// }
+
+// isEvenOrOdd(4)
+// isEvenOrOdd(9)
+
+
+function isNumber (myInput) {
+  if (Number(myInput)) {
+    console.log(`${myInput} is a number`)
   } else {
-    console.log(`o valor menor é ${c}`);
+    console.log(`${myInput} is not a number`)
   }
+  console.log(4)
 }
 
+
+// isNumber(4)
